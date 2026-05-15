08fbf35562e589a8209cfeb0c72e43d64ed11e00
Deepak Kumar Shukla <deepak.22scse1012073@galgotiasuniversity.edu.in>
2026-05-15 17:11:41 +0530

    Add: reverse-string - submission-4

---
+++ Data Structures & Algorithms/reverse-string/submission-4.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void reverseString(char[] s) {
+        // we will do it by four ways: swapping while moving till mid,
+        // two pointer, stack, recursion.
+
+        //first method
+
+        // for(int i = 0; i<s.length/2; i++){
+        //     char temp = s[i];
+        //     s[i] = s[s.length-1-i];
+        //     s[s.length-1-i] = temp;
+        // }
+
+        //second method
+        // int l = 0, r = s.length-1;
+        // while(l<r){
+        //     char temp = s[l];
+        //     s[l] = s[r];
+        //     s[r] = temp;
+        //     l++;
+        //     r--;
+        // }
+
+        //using a stack
+
+        // Stack<Character> stack = new Stack<>();
+        // for(int i = 0; i<s.length; i++){
+        //     stack.push(s[i]);
+        // }
+        // for(int i = 0;i <s.length; i++){
+        //     s[i] = stack.pop();
+        // }
+
+        //using the recursive method
+
+        reverse(0,s.length-1,s);
+
+    }
+
+    // The recursive method
+
+    public void reverse(int l, int r, char[] s){
+        if(l>=r) return;
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+        reverse(l+1,r-1,s); 
+    }
+}
